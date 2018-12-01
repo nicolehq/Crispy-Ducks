93d57794d8f49f3b259114a64ee378204a183bd8
Lizzy <lizzy960927@gmail.com>
2018-12-01 11:37:55 -0500

    i update what

--- HairMonopoly.java
+++ HairMonopoly.java
@@ -12,5 +12,6 @@ public class HairMonopoly {
     System.out.println("Work will increase your money and make you tired.");
     System.out.println("Haircare will cost you money but make you less tired.");
     System.out.println("If you are too tired, you will lose hair.");
+    //wowowowo
   }
 }
