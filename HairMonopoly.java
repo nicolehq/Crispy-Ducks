753b2ca1f081840fc89c8cb663d708edfe5bcfd9
nicolehq <nicolehq@brandeis.edu>
2018-12-01 12:49:54 -0500

    I added the pseudocode

--- HairMonopoly.java
+++ HairMonopoly.java
@@ -16,3 +16,30 @@ public class HairMonopoly {
     //hjkjgk
   }
 }
+
+/*
+60 0 60
+stats[0] = hair; stats[1] = money; stats[2] = study;
+for(int i=1;i<=7;i++){
+  losing hair(-5)
+  activity 1
+    String activity = TextIO()
+    switch
+      (hair:+10hair, -10money; work:-5hair, money:+5; study: -15hair, +10study)
+      (error)
+  activity 2
+    ''
+  calculation
+  if(i=5){
+    surprise()
+    break
+  }
+}
+
+  surprise(){
+    casino (random)
+    spa(頭部)
+    通宵學習
+  }
+
+  wig ()//
