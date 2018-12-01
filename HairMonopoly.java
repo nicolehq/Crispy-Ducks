a714d96c0a60287abb07868f7cca94744ca47662
Lizzy <44821202+LizzyAzusa@users.noreply.github.com>
2018-12-01 11:24:56 -0500

    Add files via upload

---
+++ HairMonopoly.java
@@ -0,0 +1,16 @@
+public class HairMonopoly {
+  public static void main(String[] args){
+    System.out.println("Welcome to the game of Hair Monopoly!");
+    System.out.println();
+    System.out.println("Here is the instruction of the game: ");
+    System.out.println("You are a super busy senior student, and your midterm is one week from now.");
+    System.out.println("You will also take your senior portrait on that day.");
+    System.out.println("In case to pass this game, you will need to look dashing on the picture as well as pass your exam.");
+    System.out.println("There are three activities available for you everyday: Study, work and haircare.");
+    System.out.println("You can choose two of them each day.");
+    System.out.println("Study will increase your study ability but make you tired.");
+    System.out.println("Work will increase your money and make you tired.");
+    System.out.println("Haircare will cost you money but make you less tired.");
+    System.out.println("If you are too tired, you will lose hair.");
+  }
+}
